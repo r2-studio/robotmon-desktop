0215e620178a95b94387092ad7e1e99cbab6d971
poi5305 <poi5305@gmail.com>
2017-07-15 16:08:29 +0800

    add base function for testing script

--- scripts/huuuge.js
+++ scripts/huuuge.js
@@ -1,18 +1,54 @@
 /* eslint-disable */
 
+var SCREEN_WIDTH = 0;
+var SCREEN_HEIGHT = 0;
+
+var RESIZE_WIDTH = 270;
+var RESIZE_HEIGHT = 480;
+
+function initScreenWH() {
+  if (SCREEN_WIDTH == 0 || SCREEN_HEIGHT == 0) {
+    var size = getScreenSize();
+    SCREEN_WIDTH = size[0];
+    SCREEN_HEIGHT = size[1];
+  }
+}
+// must call first
+initScreenWH();
+
+// Utils
+function toResizeXY(x, y) {
+  var rx = x * RESIZE_WIDTH / SCREEN_WIDTH;
+  var ry = y * RESIZE_HEIGHT / SCREEN_HEIGHT;
+  return [rx, ry];
+}
+
+function toRotationRXY(x, y) {
+  return [y, SCREEN_WIDTH - x];
+}
+
+function toRotationLXY(x, y) {
+  return [SCREEN_HEIGHT - y, x];
+}
+
+
+// Huuuge game
 function earn100KTask() {
   var x = 1720;
   var y = 520;
   tap(x, y);
 }
 
-function screenshot() {
+function cropAndGetMoneyNumbers() {
   console.log(Date.now());
   var img = getScreenshot();
+  var rImg = resizeImage(img, RESIZE_WIDTH, RESIZE_HEIGHT);
+  var cImg = cropImage(rImg, 970, 225, 40);
   console.log(Date.now());
-  var color = getImageColor(img, 196, 553);
-  console.log(Date.now(), color.r, color.g, color.b, color.a);//bug bgra
+  // var color = getImageColor(img, 196, 553);
+  // console.log(Date.now(), color.r, color.g, color.b, color.a);//bug bgra
   releaseImage(img);
+  releaseImage(rImg);
 }
 
 function spin() {
@@ -27,3 +63,5 @@ gTaskController.addTask('spin', spin, {priority: 10, delay:100, times: -1});
 // gTaskController.addTask('screenshot', screenshot, {priority: 0, delay:0, times: -1});
 
 // delay 100, run 300 times, 1 min, spin 50
+
+// cropAndGetMoneyNumbers();
