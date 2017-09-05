6501c5f729fbdf92929ea73dfad5c91a5d207aa0
Andy <poi5305@gmail.com>
2017-09-05 17:30:18 +0800

    add buttons mapping, task double speed, task treasure

--- scripts/EndlessFrontier/index.js
+++ scripts/EndlessFrontier/index.js
@@ -1,6 +1,8 @@
 var Config = {
   screenWidth: 0, // auto detect
   screenHeight: 0, // auto detect
+  resizeWidth: 0,
+  resizeHeight: 0,
   virtualButtonHeight: 0, // auto detect
   // isCheckTask: false,
   // isCheckTreasure: true,
@@ -9,14 +11,59 @@ var Config = {
   // isCheckDoubleSpeed: true,
   // isCheckRevolution: true,
   // revolutionMinutes: 10,
-  hasVirtualButtonBar: true,
+  hasVirtualButtonBar: false,
 };
+
+function log () {
+  sleep(100);
+  if (typeof arguments[0] == 'object') {
+    console.log(JSON.stringify(arguments[0]));
+  } else {
+    console.log.apply(console, arguments);
+  }
+}
+
+function isSameColor(c1, c2, diff) {
+  if (diff == undefined) {
+    diff = 20;
+  }
+  if (Math.abs(c1.r - c2.r) > diff) {
+    return false;
+  }
+  if (Math.abs(c1.g - c2.g) > diff) {
+    return false;
+  }
+  if (Math.abs(c1.b - c2.b) > diff) {
+    return false;
+  }
+  if (Math.abs(c1.a - c2.a) > diff) {
+    return false;
+  }
+  return true;
+}
+
+function getColor(img, xy) {
+  var xy = toResizeXYs(xy);
+  return getImageColor(img, xy.x, xy.y);
+}
+
+function toResizeXYs(xy) {
+  return toResizeXY(xy.x, xy.y);
+}
+
+function toResizeXY(x, y) {
+  var rx = Math.floor(x * Config.resizeWidth / Config.screenWidth);
+  var ry = Math.floor(y * Config.resizeHeight / Config.screenHeight);
+  return {x: rx, y: ry};
+}
 
 // init
 (function(){
   var size = getScreenSize();
   Config.screenHeight = size.height;
   Config.screenWidth = size.width;
+  Config.resizeWidth = Math.floor(Config.screenWidth / 3);
+  Config.resizeHeight =Math.floor(Config.screenHeight / 3);
   Config.virtualButtonHeight = getVirtualButtonHeight();
 })();
 
@@ -25,8 +72,38 @@ function EndlessFrontier() {
     // screen layout
     captureWidth: 1080,
     captureHeight: 1776,
+    menuHeight: 136,
+
+    InGameCheck: {x: 340, y: 820, color: {"a":0,"b":1,"g":1,"r":1}},
+    
+    ButtonMenuArmyRevolution: {x: 725, y: 920},
+    ButtonRevolutionTeam: {x: 500, y: 1180},
+    ButtonMenuStoreProp: {x: 700, y: 750},
+    ButtonTaskIcon: {x: 100, y: 1070},
+    ButtonTaskMoney: {x: 1040, y: 1070},
+    ButtonTaskMax: {x: 420, y: 1060},
+    ButtonAutoTask: {x: 1040, y: 900},
+    ButtonUnopenedTask: {x: 630, y: 1120},
+    ButtonTooLongBack: {x: 650, y: 1450},
+    ButtonDiamondFree: {x: 650, y: 1250},
+    ButtonDiamondSeeAd: {x: 460, y: 1130, color: {r: 1, g:1, b: 1, a: 0}},
+    ButtonDiamondCancel: {x: 780, y: 1130},
+    ButtonDoubleSpeed: {x: 1010, y: 1093},
+    ButtonTaskInfoCancel: {x: 620, y: 1410},
+    ButtonNetwork: {x: 650, y: 1093},
+    ButtonExitGame: {x: 820, y: 990},
+    ButtonArmyInfoCancel: {x: 1020, y: 260},
+    // AdButtonBottomRightCancel: {x: 1036, y: 1736},
+    // AdButtonTopRightCancel: {x: 1003, y: 83},
+    // AdButtonTopLeftCancel: {x: 73, y: 73},
+    // AdButtonPersonalizedOK: {x: 750, y: 820},
+    // AdBackground: {x: 160, y: 1750},
+    // AdInfoIconBottomLeft: {x: 20, y: 1756},
+    // AdInfoIconTopRight: {x: 1056, y: 20},
+    Treasure: {x: 550, y: 550},
+
     // config
-    during: 300,
+    during: 400,
   };
   this.ScreenInfo = {
     ratio: 0,
@@ -35,14 +112,7 @@ function EndlessFrontier() {
     gameWidth: 0,
   };
   // from 1776 * 1080 screen
-  this.Buttons = {
-    menuTask: {x: 90, y: 1860},
-    menuRole: {x: 270, y: 1860},
-    menuWar: {x: 450, y: 1860},
-    menuTreasure: {x: 630, y: 1860},
-    menuBattle: {x: 810, y: 1860},
-    menuShop: {x: 990, y: 1860},
-  };
+  this.Buttons = {};
   this.init();
 }
 
@@ -62,7 +132,132 @@ EndlessFrontier.prototype.init = function() {
   this.ScreenInfo.gameWidth = Config.screenWidth * gameWidthRatio;
   this.ScreenInfo.ratio = this.ScreenInfo.gameHeight / this.ScreenInfo.gameWidth;
   this.ScreenInfo.offsetX = (Config.screenWidth - this.ScreenInfo.gameWidth) / 2;
+
+  this.initButtons();
+  this.taskTreasure();
   console.log(JSON.stringify(this.ScreenInfo));
+};
+
+EndlessFrontier.prototype.getRealHeightRatio = function(v) {
+  return v * this.ScreenInfo.gameWidth / this.Const.captureWidth;
+}
+
+EndlessFrontier.prototype.getRealWHRatio = function(xy) {
+  return {
+    x: xy.x * this.ScreenInfo.gameWidth / this.Const.captureWidth, 
+    y: xy.y * this.ScreenInfo.gameWidth / this.Const.captureWidth,
+  }
+}
+
+EndlessFrontier.prototype.getRealWHRatioBottom = function(xy) {
+  var y = this.Const.captureHeight - xy.y;
+  return {
+    x: xy.x * this.ScreenInfo.gameWidth / this.Const.captureWidth, 
+    y: this.ScreenInfo.gameHeight - y * this.ScreenInfo.gameWidth / this.Const.captureWidth,
+  }
+}
+
+EndlessFrontier.prototype.initButtons = function() {
+  // Menu
+  var menuY = this.ScreenInfo.gameHeight - this.getRealHeightRatio(this.Const.menuHeight) / 2;
+  var menuW = this.ScreenInfo.gameWidth / 6;
+  var menuOffset = menuW / 2;
+  this.ButtonMenuTask = {x: (menuW * 1 - menuOffset), y: menuY};
+  this.ButtonMenuArmy = {x: (menuW * 2 - menuOffset), y: menuY};
+  this.ButtonMenuWar = {x: (menuW * 3 - menuOffset), y: menuY};
+  this.ButtonMenuTreasure = {x: (menuW * 4 - menuOffset), y: menuY};
+  this.ButtonMenuBattle = {x: (menuW * 5 - menuOffset), y: menuY};
+  this.ButtonMenuStore = {x: (menuW * 6 - menuOffset), y: menuY};
+
+  // from bottom
+  this.ButtonRevolution = {x: menuW * 5, y: menuY};
+  this.ButtonRevolutionDone = {x: menuW * 3, y: menuY};
+  this.ButtonArmyInfoCancel = this.getRealWHRatioBottom(this.Const.ButtonArmyInfoCancel);
+
+  // from top
+  this.ButtonMenuArmyRevolution = this.getRealWHRatio(this.Const.ButtonMenuArmyRevolution);
+  this.ButtonRevolutionTeam = this.getRealWHRatio(this.Const.ButtonRevolutionTeam);
+  this.ButtonMenuStoreProp = this.getRealWHRatio(this.Const.ButtonMenuStoreProp);
+  this.ButtonTaskIcon = this.getRealWHRatio(this.Const.ButtonTaskIcon);
+  this.ButtonTaskMoney = this.getRealWHRatio(this.Const.ButtonTaskMoney);
+  this.ButtonTaskMax = this.getRealWHRatio(this.Const.ButtonTaskMax);
+  this.ButtonAutoTask = this.getRealWHRatio(this.Const.ButtonAutoTask);
+  this.ButtonUnopenedTask = this.getRealWHRatio(this.Const.ButtonUnopenedTask);
+  this.ButtonTooLongBack = this.getRealWHRatio(this.Const.ButtonTooLongBack);
+  this.ButtonDiamondFree = this.getRealWHRatio(this.Const.ButtonDiamondFree);
+  this.ButtonDiamondSeeAd = this.getRealWHRatio(this.Const.ButtonDiamondSeeAd);
+  this.ButtonDiamondCancel = this.getRealWHRatio(this.Const.ButtonDiamondCancel);
+  this.ButtonDoubleSpeed = this.getRealWHRatio(this.Const.ButtonDoubleSpeed);
+  this.ButtonTaskInfoCancel = this.getRealWHRatio(this.Const.ButtonTaskInfoCancel);
+  this.ButtonNetwork = this.getRealWHRatio(this.Const.ButtonNetwork);
+  this.ButtonExitGame = this.getRealWHRatio(this.Const.ButtonExitGame);
+  this.Treasure = this.getRealWHRatio(this.Const.Treasure);
+  this.InGameCheck = this.getRealWHRatio(this.Const.InGameCheck);
+  
+};
+
+EndlessFrontier.prototype.goBack = function() {keycode('BACK', this.Const.during);}
+
+EndlessFrontier.prototype.tap = function(xy, during) {
+  if (during === undefined) {
+    during = this.Const.during;
+  }
+  console.log('tap', xy.x, xy.y);
+  tap(xy.x, xy.y, during);
+}
+
+EndlessFrontier.prototype.goToGame = function(during) {
+  if (during === undefined) {
+    during = 35 * 1000;
+  }
+  var start = Date.now();
+  while(true) {
+    log('檢查是否在遊戲中');
+    var img = this.screenshot();
+    var color = getColor(img, this.InGameCheck);
+    releaseImage(img);
+    if (isSameColor(this.Const.InGameCheck.color, color)) {
+      return;
+    }
+    if (Date.now() - start > during) {
+      return;
+    }
+    this.goBack();
+    this.tap(this.ButtonMenuTask);
+    sleep(2000);
+  }
+}
+
+EndlessFrontier.prototype.screenshot = function() {
+  return getScreenshotModify(0, 0, 0, 0, Config.resizeWidth, Config.resizeHeight);
+}
+
+// game controller
+EndlessFrontier.prototype.taskDoubleSpeed = function() {
+  log('檢查兩倍速度');
+  this.goToGame();
+  this.tap(this.ButtonMenuStore);
+  this.tap(this.ButtonMenuStoreProp);
+  this.tap(this.ButtonDoubleSpeed);
+  this.goBack();
+  this.goBack();
+  this.tap(this.ButtonMenuTask);
+};
+
+EndlessFrontier.prototype.taskTreasure = function() {
+  log('檢查自動開寶箱');
+  this.goToGame();
+  this.tap(this.Treasure);
+  // check and watch Ad
+  var img = this.screenshot();
+  var color = getColor(img, this.ButtonDiamondSeeAd);
+  releaseImage(img);
+  if (isSameColor(this.Const.ButtonDiamondSeeAd.color, color)) {
+    log('[寶箱] 是鑽石寶箱阿！！！');
+    this.tap(this.ButtonDiamondSeeAd);
+    sleep(2000);
+    goToGame();
+  }
 };
 
 new EndlessFrontier();
