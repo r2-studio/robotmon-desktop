1b891e0e7a6bf9d2befab5e3371b224bee8307b3
Shawn <hn62257@gmail.com>
2017-10-11 05:06:31 +0800

    Add concurrently

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -2,53 +2,53 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
 
-	watch: true,
+  watch: true,
 
-	target: 'electron',
+  target: 'electron',
 
-	entry: './app/src/entry.js',
+  entry: './app/src/entry.js',
 
-	output: {
-		path: __dirname + '/app/build',
-		publicPath: 'build/',
-		filename: 'bundle.js'
-	},
+  output: {
+    path: __dirname + '/app/build',
+    publicPath: 'build/',
+    filename: 'bundle.js'
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['react']
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					use: "css-loader"
-				})
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: { name: '[name].[ext]?[hash]' }
-					}
-				]
-			}
-		]
-	},
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: '[name].[ext]?[hash]' }
+          }
+        ]
+      }
+    ]
+  },
 
-	plugins: [
-		new ExtractTextPlugin({
-			filename: 'bundle.css',
-			disable: false,
-			allChunks: true
-		})
-	]
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      disable: false,
+      allChunks: true
+    })
+  ]
 
 }
