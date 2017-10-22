becb1e2a0a69d3a74e63abac09c6419d9ca627bd
poi5305 <poi5305@gmail.com>
2017-10-22 15:16:20 +0800

    Component Editor. And client to devices

---
+++ react-app/app/src/components/Editor.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Panel } from 'react-bootstrap';
+import fp from 'func-pipe';
+import _ from 'lodash';
+
+import ServiceClient from '../modules/service-client';
+import { CEditorEB } from '../modules/event-bus';
+
+import {} from '../styles/global.css';
+
+class EditorClient {
+  constructor(ip) {
+    this.ip = ip;
+    this.isConnect = false;
+    this.connectState = 'connecting...';
+
+    this.client = new ServiceClient(ip);
+    this.client.init();
+    this.testConnection();
+  }
+
+  testConnection() {
+    fp
+      .pipe(fp.bindObj(this.client.runScript, this.client, 'console.log("Robotmon Desktop Connect");'))
+      .pipe(() => {
+        this.isConnect = true;
+        this.connectState = 'connected';
+      })
+      .catch(() => {
+        this.isConnect = false;
+        this.connectState = 'disconnect';
+      })
+      .pipe(() => CEditorEB.emit(CEditorEB.EventClientChanged, this.ip));
+  }
+}
+
+export default class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      currentEditor: {},
+    };
+    this.editorClients = {};
+    this.isConnect = false;
+
+    this.onStateChange = this.onStateChange.bind(this);
+
+    CEditorEB.addListener(CEditorEB.EventClientChanged, this.onStateChange);
+  }
+
+  static get propTypes() {
+    return {
+      ip: PropTypes.string.isRequired,
+    };
+  }
+
+  componentWillUpdate(nextProps) {
+    const nextIP = nextProps.ip;
+    if (nextIP !== '' && _.isUndefined(this.editorClients[nextIP])) {
+      this.editorClients[nextIP] = new EditorClient(nextIP);
+    }
+    this.state.currentEditor = this.editorClients[nextIP];
+  }
+
+  onStateChange(ip) {
+    if (this.state.currentEditor.ip === ip) {
+      // this.forceUpdate();
+      this.setState({
+        currentEditor: this.state.currentEditor,
+      });
+    }
+  }
+
+  render() {
+    let header = 'Use Service Controller To Start Editor';
+    if (this.props.ip !== '') {
+      header = `Editor (${this.props.ip}) (${this.state.currentEditor.connectState}) `;
+    }
+    return (
+      <div>
+        <Panel header={header} />
+      </div>
+    );
+  }
+}
