151901b3b63c489b9381b2105d6b08d085246f13
Shawn <hn62257@gmail.com>
2017-11-16 17:29:48 +0800

    Update CropImage size

--- react-app/app/src/components/CropImage.jsx
+++ react-app/app/src/components/CropImage.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import {} from '../styles/global.css';
-
 export default class CropImage extends Component {
   static parsePath(filepath) {
     const paths = filepath.split('/');
@@ -27,19 +25,9 @@ export default class CropImage extends Component {
   render() {
     const pathObj = CropImage.parsePath(this.props.filepath);
     return (
-      <div>
-        <img src={this.props.src} />
-        <div>Path: {this.props.filepath}</div>
-        <div>RBM:
-          <pre>
-            rbm.findImage(&quot;{pathObj.filename}&quot;);<br />
-            rbm.imageExists(&quot;{pathObj.filename}&quot;);<br />
-            rbm.imageClick(&quot;{pathObj.filename}&quot;);<br />
-            rbm.imageWaitClick(&quot;{pathObj.filename}&quot;, 5000);<br />
-            rbm.imageWaitShow(&quot;{pathObj.filename}&quot;, 5000);<br />
-            rbm.imageWaitGone(&quot;{pathObj.filename}&quot;, 5000);<br />
-          </pre>
-        </div>
+      <div style={{ margin: 5 }}>
+        <div >{pathObj.filename}</div>
+        <img src={this.props.src} style={{ maxWidth: 200, maxHeight: 200 }} />
       </div>
     );
   }
