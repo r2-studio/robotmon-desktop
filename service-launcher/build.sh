0506cfb302dcf0b98e240195a39337aabfc43767
Shawn <hn62257@gmail.com>
2018-02-13 02:50:30 +0800

    Add DMG installer

--- service-launcher/build.sh
+++ service-launcher/build.sh
@@ -2,16 +2,17 @@ VERSION="v0.4"
 astilectron-bundler -v
 
 cd output/darwin-amd64
-zip -r "../Robotmon-Service-Manager-$VERSION-mac.zip" "Robotmon Service Manager.app"
-# cd ../linux-amd64
-# zip -r "../Robotmon-Service-Manager-$VERSION-linux.zip" "Robotmon Service Manager"
-# cd ../windows-386
-# zip -r "../Robotmon-Service-Manager-$VERSION-win32.zip" "Robotmon Service Manager.exe"
-# cd ../windows-amd64
-# zip -r "../Robotmon-Service-Manager-$VERSION-win64.zip" "Robotmon Service Manager.exe"
+electron-installer-dmg "Robotmon Service Manager.app" "Robotmon Service Manager" --icon=../../resources/icon.icns
+mv "Robotmon Service Manager.dmg" "../Robotmon-Service-Manager-$VERSION-mac.dmg"
+cd ../linux-amd64
+zip -r "../Robotmon-Service-Manager-$VERSION-linux.zip" "Robotmon Service Manager"
+cd ../windows-386
+zip -r "../Robotmon-Service-Manager-$VERSION-win32.zip" "Robotmon Service Manager.exe"
+cd ../windows-amd64
+zip -r "../Robotmon-Service-Manager-$VERSION-win64.zip" "Robotmon Service Manager.exe"
 
 cd ..
 rm -rf darwin-amd64
-# rm -rf linux-amd64
-# rm -rf windows-386
-# rm -rf windows-amd64
+rm -rf linux-amd64
+rm -rf windows-386
+rm -rf windows-amd64
