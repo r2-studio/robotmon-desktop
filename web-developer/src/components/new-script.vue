56d1245b017aef3fb5092fb8503e243ce6ad4074
poi5305 <poi5305@gmail.com>
2019-05-06 11:41:57 +0800

    add new-script pay price selection UI

--- web-developer/src/components/new-script.vue
+++ web-developer/src/components/new-script.vue
@@ -2,6 +2,8 @@
   <v-form>
     <v-layout row wrap>
       <v-flex xs12 display-1 mb-4>New Script</v-flex>
+    </v-layout>
+    <v-layout row wrap>
       <v-flex xs8 mb-2>
         <v-text-field 
           label="Script ID"
@@ -11,6 +13,8 @@
         >
         </v-text-field>
       </v-flex>
+    </v-layout>
+    <v-layout row wrap>
       <v-flex xs8 mb-2>
         <v-text-field 
           label="Display Name"
@@ -20,15 +24,19 @@
         >
         </v-text-field>
       </v-flex>
+    </v-layout>
+    <v-layout row wrap>
       <v-flex xs8 mb-2>
         <v-text-field 
-        label="Taget Game Package Name"
-        placeholder="com.linecorp.LGTMTMG"
-        hint="This effect your script icon"
-        v-model="gamePackageName"
-      >
-      </v-text-field>
+          label="Taget Game Package Name"
+          placeholder="com.linecorp.LGTMTMG"
+          hint="This effect your script icon"
+          v-model="gamePackageName"
+        >
+        </v-text-field>
       </v-flex>
+    </v-layout>
+    <v-layout row wrap>
       <v-flex xs8 mb-2>
         <v-textarea 
           label="Description"
@@ -37,11 +45,48 @@
         >
         </v-textarea>
       </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-radio-group row v-model="payPlan" @change="onPayClick">
+        <v-radio :label="`Free`" :value="0"></v-radio>
+        <v-radio :label="`Pay`" :value="2"></v-radio>
+      </v-radio-group>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs11 mb-2>
+       <v-slider 
+        v-model="payPeriodSelection"
+        label="Period"
+        value="5"
+        min="0"
+        max="5"
+        :disabled="payPlan==0"
+        :tick-labels="periodLabels">
+      </v-slider>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs11 mb-2>
+       <v-slider 
+        v-model="payPrice"
+        label="Price Points" 
+        value="0"
+        step="5"
+        min="0"
+        max="300"
+        :disabled="payPlan==0"
+        thumb-label="always">
+      </v-slider>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
       <v-flex xs8 mb-2 v-if="alert != ''">
         <v-alert :value="true" type="warning" >
           {{alert}}
         </v-alert>
       </v-flex>
+    </v-layout>
+    <v-layout row wrap>
       <v-flex xs6></v-flex>
       <v-flex xs4>
         <v-btn color="info" @click="newScript">New Script</v-btn>
@@ -63,6 +108,26 @@ module.exports = {
       displayName: "",
       gamePackageName: "",
       description: "",
+      payPlan: 0,
+      payPrice: 0,
+      payPeriod: 0,
+      payPeriodSelection: 5,
+      periodLabels: [
+        '1H (One Time)',
+        '1 Day',
+        '7 Day',
+        '30 Day',
+        '3 Month',
+        '1 Year',
+      ],
+      periodMappingTime: [
+        3600 * 1000,
+        24 * 3600 * 1000,
+        7 * 24 * 3600 * 1000,
+        30 * 24 * 3600 * 1000,
+        90 * 24 * 3600 * 1000,
+        365 * 24 * 3600 * 1000,
+      ],
     };
   },
   methods: {
@@ -93,8 +158,27 @@ module.exports = {
         ok = false;
         this.alert += 'Discription not set, ';
       }
+      if (this.payPlan === 0) {
+        this.payPeriodSelection = 5;
+        this.payPrice = 0;
+      } else if (this.payPlan === 2 && this.payPrice === 0) {
+        ok = false;
+        this.alert += 'Price cloud not be 0 when on Pay Plan';
+      }
+      if (ok) {
+        this.payPeriod = this.periodMappingTime[this.payPeriodSelection];
+      }
       return ok;
     },
+    onPayClick: function() {
+      if (this.payPlan === 0) {
+        this.payPeriodSelection = 5;
+        this.payPrice = 0;
+      } else if (this.payPlan === 2) {
+        this.payPeriodSelection = 3;
+        this.payPrice = 30;
+      }
+    }
   },
 };
 </script>
