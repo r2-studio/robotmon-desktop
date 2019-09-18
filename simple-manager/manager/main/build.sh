2b05331edb5cab34eb0b0f4b9ea75114c0efc034
poi5305 <poi5305@gmail.com>
2019-09-18 15:58:12 +0800

    update tag position

--- simple-manager/manager/main/build.sh
+++ simple-manager/manager/main/build.sh
@@ -4,7 +4,7 @@ rm -r releases
 mkdir releases
 
 packr -z -v
-GOOS=darwin GOARCH=amd64 go build && mv ./main ./releases/simple-manager-drawin-$tag
-GOOS=linux GOARCH=amd64 go build && mv ./main ./releases/simple-manager-linux-$tag
-GOOS=windows GOARCH=amd64 go build && mv ./main.exe ./releases/simple-manager-windows-$tag.exe
+GOOS=darwin GOARCH=amd64 go build && mv ./main ./releases/simple-manager-$tag-drawin
+GOOS=linux GOARCH=amd64 go build && mv ./main ./releases/simple-manager-$tag-linux
+GOOS=windows GOARCH=amd64 go build && mv ./main.exe ./releases/simple-manager-$tag-windows.exe
 packr clean
