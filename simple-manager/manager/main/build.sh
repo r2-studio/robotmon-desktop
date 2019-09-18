b77dec6d4bc98e8ff6b6758acd0e7aa8bb51b7e1
poi5305 <poi5305@gmail.com>
2019-09-18 15:53:31 +0800

    build with version

--- simple-manager/manager/main/build.sh
+++ simple-manager/manager/main/build.sh
@@ -1,9 +1,10 @@
+tag=`git describe --tags HEAD`
+
 rm -r releases
 mkdir releases
 
 packr -z -v
-
-GOOS=darwin GOARCH=amd64 go build && mv ./main ./releases/simple-manager-drawin
-GOOS=linux GOARCH=amd64 go build && mv ./main ./releases/simple-manager-linux
-GOOS=windows GOARCH=amd64 go build && mv ./main.exe ./releases/simple-manager-windows.exe
+GOOS=darwin GOARCH=amd64 go build && mv ./main ./releases/simple-manager-drawin-$tag
+GOOS=linux GOARCH=amd64 go build && mv ./main ./releases/simple-manager-linux-$tag
+GOOS=windows GOARCH=amd64 go build && mv ./main.exe ./releases/simple-manager-windows-$tag.exe
 packr clean
