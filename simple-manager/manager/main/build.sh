e4c27ad645be50bf987c55203597860268a23da8
poi5305 <poi5305@gmail.com>
2020-03-08 12:01:21 +0800

    build windows 32bits

--- simple-manager/manager/main/build.sh
+++ simple-manager/manager/main/build.sh
@@ -6,5 +6,6 @@ mkdir releases
 packr -z -v
 GOOS=darwin GOARCH=amd64 go build && mv ./main ./releases/simple-manager-$tag-drawin
 GOOS=linux GOARCH=amd64 go build && mv ./main ./releases/simple-manager-$tag-linux
-GOOS=windows GOARCH=amd64 go build && mv ./main.exe ./releases/simple-manager-$tag-windows.exe
+GOOS=windows GOARCH=amd64 go build && mv ./main.exe ./releases/simple-manager-$tag-windows-64.exe
+GOOS=windows GOARCH=386 go build && mv ./main.exe ./releases/simple-manager-$tag-windows-32.exe
 packr clean
